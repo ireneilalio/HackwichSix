866bd5468257f865480bde4494d3937fdb4d800d
Irene Ilalio <irenei@hawaii.edu>
2022-10-18 22:56:36 -1000

    Completed parts 1-9 and problem set 1

--- HackwichSix/ViewController.swift
+++ HackwichSix/ViewController.swift
@@ -6,14 +6,43 @@
 //
 
 import UIKit
+//Part6: we add 2 delegates UITableViewDelegate and UITableViewDataSource and it configures 2 functions below class
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    //Part7: declare an array of strings; name it myFriends
+    var myFriendsArray = ["Cindy", "Tracy", "Shervon"]
+    
+    var myCitiesArray = ["Italy", "Spain", "Portugal", "Denmark", "Dublin"]
+    
+    
+    override func viewDidLoad(){
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+        
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        //Part8: 4 lines of code that create the cell and fill it with data
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier")!
+        //displays text in each row in Tableview
+        let text = myCitiesArray[indexPath.row]
+        cell.textLabel?.text = text
+        
+        return cell
+    }
+        
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+        {
+            //Part 8:  communicates to tableview how many rows to tableview. it will return the count whatever you have in the myFriends var
+            return myCitiesArray.count
 
-
-}
+        }
+     
+        
+    }
+        
+        
+    
 
