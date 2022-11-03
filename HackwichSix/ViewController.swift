7432d3e45ad90a5ea19bcb0a6109faa8444e75d6
Irene Ilalio <irenei@hawaii.edu>
2022-11-02 20:14:45 -1000

    Completed Hackwich 10 Parts 1-12 and Problem Set #1

--- HackwichSix/ViewController.swift
+++ HackwichSix/ViewController.swift
@@ -10,18 +10,35 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     //Part7: declare an array of strings; name it myFriends
-    var myFriendsArray = ["Cindy", "Tracy", "Shervon"]
+    //var myFriendsArray = ["Cindy", "Tracy", "Shervon"]
+    
+    //Hackwich 10: Problem Set #1: change my friends array
+    var myFriendsArray = ["Kapolei Kalapawai"]
     
     var myCitiesArray = ["Italy", "Spain", "Portugal", "Denmark", "Dublin"]
     
-    var friendsHomeArray = ["Makakilo", "Waipio", "Kaneohe"]
+    //var friendsHomeArray = ["Makakilo", "Waipio", "Kaneohe"]
+    //Part7-2
+    var restaurantImageData = [String]()
     
     override func viewDidLoad(){
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //Part7-1: a:create NSBundle object that enables us to create a pathway tou our pList; b: initialize a dictionary using the keys and values
+        let path = Bundle.main.path(forResource: "Property List", ofType: "plist")
+        let dict = NSDictionary(contentsOfFile: path!)
+        
+        //Part7-3
+        restaurantImageData = dict!.object(forKey: "restaurantImages")
+        as! [String]
     }
         
+    
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
     {
@@ -30,9 +47,15 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         //displays text in each row in Tableview
         let text = myFriendsArray[indexPath.row]
         cell.textLabel?.text = text
-        cell.detailTextLabel?.text = friendsHomeArray[indexPath.row]
+        //cell.detailTextLabel?.text = friendsHomeArray[indexPath.row]
         return cell
     }
+    //Part8-1: add function didselectrowat
+    //Part8-2: add one line of code in the funtions code block that indicates that a row has been selected
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath as IndexPath, animated: true)
+    }
+    
         
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
         {
@@ -40,7 +63,16 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
             return myFriendsArray.count
 
         }
-     
+     //Part12: code statements to pass the data from the pList through the Segue:
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "mySegue"
+        {
+            let s1 = segue.destination as! detailViewController
+            let imageIndex = tableView.indexPathForSelectedRow?.row
+            s1.imagePass = restaurantImageData[imageIndex!]
+        }
+            
+    }
         
     }
         
---
+++ HackwichSix/detailViewController.swift
@@ -0,0 +1,42 @@
+//
+//  detailViewController.swift
+//  HackwichSix
+//
+//  Created by Irene Ilalio on 11/2/22.
+//
+
+import UIKit
+
+class detailViewController: UIViewController {
+    
+   //Part10-d: create IBOutlet for imageview
+    @IBOutlet weak var imageView: UIImageView!
+    
+    //Part11-1
+    var imagePass: String?
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //Part11-2
+        if let imageName = imagePass {
+            imageView.image = UIImage(named: imageName)
+        }
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
